7f94c1e65e5e7106a6555ff4dca43a2d44ec7ccc
lucapitzoi <luca.pitzoi@sindria.org>
2020-05-12 01:24:57 +0200

    Added cron and supervisor package, Added custom CMD on Dockerfie

---
+++ src/cmd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+# Override cron.d configuration
+if [ -e /home/sindria/config/cron.d/app ]; then
+    cp /home/sindria/config/cron.d/app /etc/cron.d/
+fi
+
+# Fix permission
+chown root:root /etc/cron.d/*
+chmod 644 /etc/cron.d/*
+chown -R sindria:sindria /var/www/app
+
+# Override timezone by env
+if [ "$TZ" != "" ] || [ "$TZ" != "Europe/Rome" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+fi
+
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
